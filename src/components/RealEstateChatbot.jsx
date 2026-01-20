fdf40d7fa9eab5b6ca4d50556ed7d10542b8c287
심상옥 <sangogsim@gmail.com>
2026-01-20 15:40:28 +0900

    fix: Update image comparison URL to correct gallery URL

--- src/components/RealEstateChatbot.jsx
+++ src/components/RealEstateChatbot.jsx
@@ -335,7 +335,7 @@ const RealEstateChatbot = () => {
     const handleStart = () => setViewState('categories');
 
     const handleOpenImageApp = () => {
-        window.open('https://unicity-iota.vercel.app', '_blank');
+        window.open('https://yeoyeo-gallery-v2.vercel.app', '_blank');
     };
 
     const handleCategorySelect = (categoryKey) => {
